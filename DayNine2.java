a4236939de1e390162bc3c952587bbd327c15e36
TARUNIMA <115608975+tarunimasuresh-111@users.noreply.github.com>
2023-06-06 15:59:58 +0530

    Add files via upload

---
+++ DayNine2.java
@@ -0,0 +1,25 @@
+package sample.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class DayNine2
+{
+    public int add(int a,int b)
+    {
+    	return (a+b);
+    }
+    public int sub(int a,int b)
+    {
+    	return (a-b);
+    }
+    public int multiply(int a,int b)
+    {
+    	return (a*b);
+    }
+    public int divide(int a,int b)
+    {
+    	return (a/b);
+    }
+}
